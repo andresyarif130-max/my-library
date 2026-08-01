fd5e738c5a112468e10ee46b989022b18e36392c
Andre Syarif <andresyarif130@gmail.com>
2026-08-01 16:32:54 +0800

    forget = and new book

--- script.js
+++ script.js
@@ -10,4 +10,6 @@ function Book(title, author, pages, read) {
   };
 }
 
-const theHobbit('The Hobbit', 'J.R.R. Tolkien', 295, false);
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+
+    console.log(theHobbit.);
