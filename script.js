747f0147284732a892c042923e25bb22100377fd
Andre Syarif <andresyarif130@gmail.com>
2026-08-17 18:11:53 +0800

    make info function as a methode

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-
-  this.info = function () {
-    const readStatus = this.read ? 'read' : 'not read yet';
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
-  };
 }
+
+Book.prototype.info = function () {
+  const readStatus = this.read ? 'read' : 'not read yet';
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
+};
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 
