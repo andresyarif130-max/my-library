c40cc930ab118a586d1d635d03faec53054b6b02
Andre Syarif <andresyarif130@gmail.com>
2026-08-17 18:17:29 +0800

    Create library array and add books

--- script.js
+++ script.js
@@ -9,7 +9,14 @@ Book.prototype.info = function () {
   const readStatus = this.read ? 'read' : 'not read yet';
   return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
 };
-
+// Test for Terminal
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 
 console.log(theHobbit.info());
+// End test for Terminal
+const myLibrary = [];
+myLibrary.push(theHobbit);
+
+// Test for Terminal
+const atomicHabits = new Book('Atomic Habits', 'James Clear', 320, true);
+myLibrary.push(atomicHabits);
