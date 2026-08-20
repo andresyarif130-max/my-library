add57d6618cfd27059ba8618df98c9baa88f85ea
Andre Syarif <andresyarif130@gmail.com>
2026-08-20 10:23:43 +0800

    Add unique IDs to books

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const bookGrid = document.querySelector('#bookGrid');
 
 function Book(title, author, pages, read) {
+  this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
