584fece79ed15543e573c2f7cfc7807b1030bd33
Andre Syarif <andresyarif130@gmail.com>
2026-08-20 14:05:14 +0800

    Delete Test book form values

--- script.js
+++ script.js
@@ -89,11 +89,6 @@ bookForm.addEventListener('submit', (event) => {
   const author = authorInput.value;
   const pages = Number(pagesInput.value);
   const read = readInput.checked;
-
-  console.log(title);
-  console.log(author);
-  console.log(pages);
-  console.log(read);
 });
 
 displayBooks();
